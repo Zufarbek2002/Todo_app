a9ec76f8c80b0514d51c164b29072d9b80722243
Zufarbek2002 <game123qwerty00@gmail.com>
2024-03-13 11:04:07 +0500

    added delete btn;

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [dataList, setDataList] = useState([]);
   const [page, setPage] = useState(1)
-  const [dataId, setDataId] = useState(0)
+  // const [dataId, setDataId] = useState(0)
   const getTodos = async () => {
-    const data = await axios(`http://localhost:3000/todos?_page=${page}&_per_page=10`);
-    // console.log(data.data.data);
-    setDataList(data.data.data);
+    const res = await axios(`http://localhost:3000/todos?_page=${page}&_per_page=10`);
+    const data = res.data
+    setDataList(data.data);
   };
   const handlePrev = () => {
     setPage(page-1)
@@ -16,32 +16,26 @@ const App = () => {
   const handleNext = () => {
     setPage(page+1)
   }
-  const handleCheck = (e) => {
-    console.log(e.target.checked);
-    console.log(e.target.value);
-    setDataId()
-    dataList.map((data)=>{
-      if (e.target.checked) {
-        data.completed = true
-      }
 
-    })
+  const handleDelete = async(id) => {
+    const res = await axios.delete(`http://localhost:3000/todos/${id}`);
   }
-
   useEffect(() => {
     getTodos();
-  }, [page]);
+    handleDelete()
+  }, [page, dataList]);
   return (
     <div className="container mt-4">
-      {dataList.map((data) => (
+      {
+      dataList.map((data) => (
         <div key={data.id} className="card mb-4">
           <div className="card-body d-flex justify-content-between">
             {/* <input type="checkbox" name="check" id="check" onClick={handleCheck}/> */}
-            <h2 className={data.completed ? 'text-decoration-underline' : 'fs-2'}>{data.title}</h2>
+            <h2 className={data.completed ? 'text-decoration-line-through fs-4' : 'fs-4'}>{data.title}</h2>
             <p>{data.completed ? "✅" : "❌"}</p>
           </div>
           <div className="card-footer">
-            <button onClick={()=>handleCheck(data.id)}>Complete</button>
+            <button >Complete</button>
             <button onClick={()=>handleDelete(data.id)}>Delete</button>
             <button>Edit</button>
           </div>
